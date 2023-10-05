cef023afeaccd707b628f5eb442ccb30a7ff635b
Kvezac <Kvezak@gmail.com>
2023-10-05 17:16:37 +0300

    create models, migrations

--- user/models.py
+++ user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    GENDER_CHOICES = [
+        ('Male', 'Мужской'),
+        ('Female', 'Женский'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=155, blank=True, null=True)
+    middle_name = models.CharField(max_length=155, blank=True, null=True)
+    last_name = models.CharField(max_length=155, blank=True, null=True)
+    gender = models.CharField(max_length=7, choices=GENDER_CHOICES, blank=True, null=True)
+    date_of_bth = models.DateField(auto_now=False, blank=True, null=True)
+    email = models.EmailField(max_length=500, blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    city = models.CharField(max_length=155, blank=True, null=True)
+
+    def __str__(self):
+        return f'Profile: {self.last_name} {self.name} {self.middle_name}'
--- user/views.py
+++ user/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def user_list(request):
+    context = User.objects.all()
+    return render(request, 'user/user-list.html', context)
