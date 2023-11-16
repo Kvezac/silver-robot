802fcb78e466621d98bbb84c4da5ef9c33d47ac7
Kvezac <Kvezak@gmail.com>
2023-11-16 18:05:42 +0300

    modified:   company/urls.py 	modified:   company/views.py 	modified:   templates/block/header.html 	modified:   templates/block/navbar.html 	modified:   templates/company/edit_employee.html 	modified:   templates/company/list_employee.html 	new file:   templates/company/list_employee_all.html 	modified:   templates/company/list_employee_id.html

--- company/views.py
+++ company/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count, Avg, Min, Max, Sum
 from django.shortcuts import render, get_object_or_404, redirect
 
 from company.forms import EmployeeForms
@@ -6,24 +7,51 @@ from company.models import Employee
 
 
 def home(request):
-    title = 'main page'
+    title = 'Страница входа'
     context = {'title': title}
     return render(request, 'company/index.html', context)
 
 
 # @login_required
 def list_employee(request):
-    title = 'Все сотрудники'
-    employees = Employee.objects.all()
+    """
+        The main page shows company statistics
+    """
+    title = 'Главная страница'
+    levels = Employee.objects.all().count()
+    managers = Employee.objects.exclude(parent=True).count()
+    avg_subordinates = Employee.objects.aggregate(avg=Avg('children'))['avg']
+    stats = Employee.objects.aggregate(min_salary=Min('salary'), max_salary=Max('salary'), sum_salary=Sum('salary'),
+                                       avg_salary=Avg('salary'))
+    min_hire_date = Employee.objects.exclude(hire_date=None).aggregate(min=Min('hire_date'))['min']
+    max_hire_date = Employee.objects.exclude(hire_date=None).aggregate(max=Max('hire_date'))['max']
+    longest_employment = Employee.objects.filter(hire_date=min_hire_date).first()
+    shortest_employment = Employee.objects.filter(hire_date=max_hire_date).first()
     context = {'title': title,
-               'employees': employees
+               'levels': levels,
+               'managers': managers,
+               'avg_subordinates': avg_subordinates,
+               'stats': stats,
+               'longest_employment': longest_employment,
+               'shortest_employment': shortest_employment,
+
                }
     return render(request, 'company/list_employee.html', context=context)
 
 
+def list_employee_all(request):
+    nodes = Employee.objects.all()
+    title = 'Все сотрудники'
+    context = {
+        'title': title,
+        'nodes': nodes,
+    }
+    return render(request, 'company/list_employee_all.html', context)
+
+
 def list_employee_id(request, level):
     nodes = Employee.objects.filter(level=level)
-    title = f'Лист сотрудников департамента уровня {level}'
+    title = f'Департамент уровнь {level}'
     # root_employee_id = current_employee.get_level()
     # print(root_employee_id)
     # nodes = Employee.objects.all()
@@ -57,4 +85,8 @@ def search_results(request):
 
 
 def change_boss(request):
+    return None
+
+
+def list_employee_all(request):
     return None
